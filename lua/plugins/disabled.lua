b8e01ef2bde02dc0fea5898dcfe8679a3dbd6134
m1ng24 <117571344+m1ng24@users.noreply.github.com>
2023-09-22 20:49:20 +0700

    initial config

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,11 @@
+return {
+
+  {
+    "goolord/alpha-nvim",
+    enabled = false,
+  },
+  { "folke/persistence.nvim", enabled = false },
+  { "folke/edgy.nvim", enabled = false },
+  { "stevearc/dressing.nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+}
