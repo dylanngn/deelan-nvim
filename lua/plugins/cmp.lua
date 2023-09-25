5fbb812b5bafe09aa804c580561adb6d303438cc
m1ng24 <117571344+m1ng24@users.noreply.github.com>
2023-09-25 11:21:00 +0700

    fix: jumable locally failed

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -21,9 +21,8 @@ return {
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
-            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
             cmp.select_next_item()
-          elseif luasnip.expand_or_locally_jumpable_jumpable() then
+          elseif luasnip.expand_or_jumpable() then
             luasnip.expand_or_jump()
           elseif has_words_before() then
             cmp.complete()
