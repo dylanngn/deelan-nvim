a3a795e3e7bab3d50b77db643c793ca0772a9ffa
m1ng24 <117571344+m1ng24@users.noreply.github.com>
2023-10-09 21:02:57 +0700

    lastest

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -5,6 +5,4 @@ return {
     enabled = false,
   },
   { "folke/persistence.nvim", enabled = false },
-  { "folke/edgy.nvim", enabled = false },
-  { "akinsho/bufferline.nvim", enabled = false },
 }
